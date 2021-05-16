9c8e6240d9e4740e104429c1075652d47064756a
An39nA0219 <an39na10282004@yahoo.co.jp>
2021-05-16 23:16:46 +0900

    count up & switch display処理

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [display, setDisplay] = useState(false);
+  const onClickCountUp = () => {
+    setCount(count + 1);
+  };
+  const onClickSwitchDisplay = () => {
+    setDisplay(!display);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        {count}! {display && <span>🤪</span>}
+      </p>
+      <button onClick={onClickCountUp}>Count UP!</button>
+      <button onClick={onClickSwitchDisplay}>On / Off</button>
     </div>
   );
 }
